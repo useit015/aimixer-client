35cd501c5ac19dbf81ec298ae3b12919115af0b4
Oussama Nahiz <useit015@gmail.com>
2023-10-03 22:55:48 +0100

    Added automatic modes: `dev`, `prod`

--- src/store/sliceServers.js
+++ src/store/sliceServers.js
@@ -1,17 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const sliceServers = createSlice({
-    name: 'servers',
-    initialState: {mode: 'prod', api: {dev: 'https://api.aimixer.io:5300', prod: 'https://api.aimixer.io:5000'}},
-    reducers: {
-        spinnerSetStatus: (state, action) => {
-            state.status = action.payload;
-            return state;
-        },
-
+  name: 'servers',
+  initialState: {
+    mode: import.meta.env.MODE === 'development' ? 'dev' : 'prod',
+    api: {
+      dev: 'https://localhost:5300',
+      prod: 'https://api.aimixer.io:5000'
+    },
+    assets: {
+      dev: 'https://assets.aimixer.io:5002',
+      prod: 'https://assets.aimixer.io:5002'
     }
+  },
+  reducers: {
+    spinnerSetStatus: (state, action) => {
+      state.status = action.payload;
+      return state;
+    }
+  }
 });
 
-export const { } = sliceServers.actions;
+export const {} = sliceServers.actions;
 
 export default sliceServers.reducer;
