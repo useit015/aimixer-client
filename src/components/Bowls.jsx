75370739d5b339b08b1711f3c0086bb65c712a7e
Woodney Juste <77648035+woodneydev@users.noreply.github.com>
2023-10-04 15:04:04 -0400

    added ability to filter user bowls by clicking on person icon

--- src/components/Bowls.jsx
+++ src/components/Bowls.jsx
@@ -6,21 +6,24 @@ import { store } from '../store/configStore';
 import * as socketService from '../socketService';
 import { IonButton, IonFab, IonFabButton, IonIcon, IonInput, IonItem } from '@ionic/react';
 import { add } from 'ionicons/icons';
+import { person } from 'ionicons/icons';
 import BowlCard from './BowlCard';
 import { toastSet } from '../store/sliceToast';
 
 function Bowls() {
     const [name, setName] = useState('')
+    const [displayByUser, setDisplayByUser] = useState(false);
 
     const login = useSelector(state => state.login);
     const bowls = useSelector(state => state.bowls);
     const servers = useSelector(state => state.servers)
+    const userBowls = bowls.filter(b => b.creator === login.email)
 
     const dispatch = useDispatch();
-
+    
     console.log('login', login);
     console.log('bowls', bowls);
-
+    console.log("user bowls", userBowls)
 
     socketService.setupTheSocket(io, `${servers.api[servers.mode]}`, store);
 
@@ -38,11 +41,18 @@ function Bowls() {
       setName('');
     }
 
+    const filterBowls = () => {
+      setDisplayByUser(prev => !prev);
+    }
+
     return (
     <div className='Bowls'>
       <h1 className="Bowls__Title">Bowls</h1>
         <div className="Bowls__New-Container">
-         
+          <IonButton onClick={filterBowls}>
+            <IonIcon icon={person} ></IonIcon>
+          </IonButton>
+
           <IonItem className='Bowls__Name'>
               <IonInput  label="New Bowl" labelPlacement="floating" placeholder="Enter name" value={name} onInput={(e) => {
                 setName(e.target.value)
@@ -51,13 +61,22 @@ function Bowls() {
           <IonButton onClick={addBowl}>
             <IonIcon icon={add} ></IonIcon>
           </IonButton>
-        </div>      
-        <div className="Bowls__List">
-            { bowls.map(b => {
-              return <BowlCard key={b.id} bowl={b} />
-            })
-            }
-          </div>
+        </div>
+        {displayByUser ? 
+            <div className="Bowls__List">
+              { bowls.map(b => {
+                  return <BowlCard key={b.id} bowl={b} />
+              })
+              }
+            </div>
+        :
+            <div className="Bowls__List">
+                { userBowls.map(b => {
+                  return <BowlCard key={b.id} bowl={b} />
+                })
+                }
+            </div>
+        }      
     </div>
   )
 }
